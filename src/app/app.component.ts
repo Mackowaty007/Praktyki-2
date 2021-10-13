2adbae7c33234428378a5430d5827210cea7a8db
Mateusz PIentka <pietka00@gmail.com>
2021-10-13 16:10:39 +0200

    Hope this works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "podaj swoje imie, a ja je zwroce!"
-  currentVal = "";
-  getVal(val: string){
-    this.currentVal = val
+  title = "kliknij przycisk aby uzyskać losowe zdjęcie"
+  request(){
+    var randomID:number = Math.floor(Math.random() * 5000);
+
+    var pathToPhoto = 'https://jsonplaceholder.typicode.com/photos/' + randomID.toString()
+
+    fetch(pathToPhoto)
+    .then(function(response){
+      return response.json()
+    })
+    .then((response) => {
+      console.log(response)
+      var result = document.getElementById('result')
+      if(result != null){
+        result.setAttribute( 'src', response.url);
+      }
+    })
   }
 }
