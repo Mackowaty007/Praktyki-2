d26f2524cac087474587e9b0630cc3e4cdddbf5e
Mateusz PIentka <pietka00@gmail.com>
2021-10-20 11:11:48 +0200

    why does this code not work?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,36 +10,16 @@ export class AppComponent {
   request(){
     var idPosta = (<HTMLInputElement>document.getElementById("myText")).value;
     console.log(idPosta)
-    var pathToPhoto = 'https://jsonplaceholder.typicode.com/comments/1'
+    var path = 'https://localhost:5001/WeatherForecast/0'
     if(idPosta != null){
-      pathToPhoto = 'https://jsonplaceholder.typicode.com/comments/' + idPosta.toString()
+      path = 'https://localhost:5001/WeatherForecast' + idPosta.toString()
     }
-    fetch(pathToPhoto)
+    fetch(path)
     .then(function(response){
       return response.json()
     })
     .then((response) => {
       console.log(response)
-      var postId = document.getElementById('postId')
-      var id = document.getElementById('id')
-      var name = document.getElementById('name')
-      var email = document.getElementById('email')
-      var body = document.getElementById('body')
-      if(postId != null ){
-        postId.innerHTML = response.postId
-      }
-      if(id != null ){
-        id.innerHTML = response.id
-      }
-      if(name != null ){
-        name.innerHTML = response.name
-      }
-      if(email != null ){
-        email.innerHTML = response.email
-      }
-      if(body != null ){
-        body.innerHTML = response.body
-      }
     })
   }
 }
